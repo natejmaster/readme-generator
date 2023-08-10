41ae76c06b668adabec9d51372da864aa770e8b9
natejmaster@gmail.com <natejmaster@gmail.com>
2023-08-10 18:44:54 -0500

    Added basic framework of readme template, threw in filler questions to question array

--- index.js
+++ index.js
@@ -1,10 +1,61 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name (first and last)?',
+    },
+    {
+        type: 'input',
+        name: 'location',
+        message: 'Where are you located? (format: city, state)',
+    },
+    {
+        type: 'input',
+        name: 'linkedin',
+        message: 'What is your LinkedIn URL address?',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub URL address?',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    const readmeContent = `
+    #${project-title}
+
+    ## Description
+    ${description}
+
+    ## Table of Contents
+    ${}
+
+    ## Installation
+    ${}
+
+    ## Usage
+    ${}
+
+    ## License
+    ${}
+
+    ## Contributing
+    ${}
+
+    ## Tests
+    ${}
+
+    ## Questions
+    ${}
+
+    `
+}
 
 // TODO: Create a function to initialize app
 function init() {}
