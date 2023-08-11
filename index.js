f96b583934ccf61d53455a25da690c39b4c60082
natejmaster@gmail.com <natejmaster@gmail.com>
2023-08-10 19:46:06 -0500

    Added more questions to inquirer prompts, developed readme template

--- index.js
+++ index.js
@@ -5,60 +5,101 @@ const fs = require('fs');
 const questions = [
     {
         type: 'input',
-        name: 'name',
-        message: 'What is your name (first and last)?',
+        name: 'projectTitle',
+        message: 'What is the title of your project?',
     },
     {
         type: 'input',
-        name: 'location',
-        message: 'Where are you located? (format: city, state)',
+        name: 'descriptionWhat',
+        message: 'What does your project do, or in other words, what problem does your project solve?',
     },
     {
         type: 'input',
-        name: 'linkedin',
-        message: 'What is your LinkedIn URL address?',
+        name: 'descriptionWhy',
+        message: 'What was your motivation in this project? Why did you build it?',
     },
     {
         type: 'input',
-        name: 'github',
-        message: 'What is your GitHub URL address?',
+        name: 'installation',
+        message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use of your project',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use of your project',
+    },
+    {
+        type: 'input',
+        name: 'collaborators',
+        message: 'Provide the names of any contributors who worked on this project with you (if this was an independent project, leave this blank)'
+    },
+    {
+        type: 'input',
+        name: 'assets',
+        message: 'Provide the names of any third-party assets you need to give credit to',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide directions for any testing of the project or application that need to be completed'
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?'
     },
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     const readmeContent = `
-    #${project-title}
+    #${projectTitle}
 
     ## Description
-    ${description}
+    ${descriptionWhat}
+    ${descriptionWhy}
 
     ## Table of Contents
-    ${}
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [License](#license)
+    - [Contributing](#contributing)
+    - [Tests](#tests)
+    - [Questions](#questions)
 
     ## Installation
-    ${}
+    ${installation}
 
     ## Usage
-    ${}
+    ${usage}
 
     ## License
     ${}
 
     ## Contributing
-    ${}
+    Other developers who collaborated with me on this project: ${collaborators}
+    Outside assets I used on this project: ${assets}
 
     ## Tests
-    ${}
+    ${tests}
 
     ## Questions
-    ${}
-
+    Do you still have questions or further inquiries? Reach out to me on GitHub at http://github.com/${username} or e-mail me at ${email}
     `
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
